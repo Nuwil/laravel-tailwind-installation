310dc624a7f4b62241bec93b3fb21cf706c203cb
Nuwil <153420007+Nuwil@users.noreply.github.com>
2026-03-25 00:27:56 +0000

    added home blade preview

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Home</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -10,6 +11,11 @@
     <div class="container mx-auto p-6">
         <h1 class="text-3xl font-bold mb-4">HELLO, WORLD!</h1>
         <p class="text-lg">I am Noel Gaddi, a passionate <span>developer.</span></p>
+    </div>
+    <div class="">
+        <a href="https://facebook.com/yourpage" class="fa fa-facebook" target="_blank"></a>
+        <a href="https://twitter.com/yourpage" class="fa fa-twitter" target="_blank"></a>
+        <a href="https://instagram.com/yourpage" class="fa fa-instagram" target="_blank"></a>   
     </div>
 </body>
 </html>
